f5b759c5ea492773ad670fb6c4ae4509459fa074
Tom Aldcroft <taldcroft@gmail.com>
2012-11-19 16:59:52 -0500

    Simplify / improve (?) plot_sim_fail.py

--- plot_sim_fail.py
+++ plot_sim_fail.py
@@ -5,43 +5,30 @@ import asciitable
 
 N_ROW_COL = 22
 lines = []
-for filename in glob.glob('reports/10+0_half_exemplar/1/act_fail-*.dat'):
+for filename in glob.glob('reports/10+0_half_exemplar/21/act_fail-*.dat'):
     lines.extend(open(filename, 'r').readlines())
 
 vals = asciitable.read(lines, Reader=asciitable.NoHeader, guess=False,
-                       names=['n_fail', 'hpd', 'rmsd', 'i_fail'])
+                       names=['n_fail', 'hpd', 'rmsd', 'a', 'b', 'i_fail'])
 n_vals = len(vals)
 i_fails_list = [np.array([int(strval) for strval in x.split(',')])
                 for x in vals['i_fail']]
 
-n_edge = np.zeros(n_vals)
-n_edge2 = np.zeros(n_vals)
-nrc1 = N_ROW_COL - 1
-for i, i_fails in enumerate(i_fails_list):
-    row = i_fails / N_ROW_COL
-    col = i_fails % N_ROW_COL
-    edge = (row < 1) | (row > nrc1 - 1) | (col < 1) | (col > nrc1 - 1)
-    edge2 = (row < 2) | (row > nrc1 - 2) | (col < 2) | (col > nrc1 - 2)
-    n_edge[i] = np.sum(edge)
-    n_edge2[i] = np.sum(edge2)
-
 xr = (np.random.random(n_vals) - 0.5) * 1
 yr = (np.random.random(n_vals) - 0.5) * 0.02
-xvals = vals['n_fail'] + xr
-yvals = vals['hpd'] + yr
+xvals = (vals['n_fail'] + xr) / 441. * 100.0
+yvals = (vals['hpd'] + yr) / 0.61
 
+plt.figure(1, figsize=(5, 3.5))
 plt.clf()
 plt.plot(xvals, yvals, ',', mec='b', mfc='b')
-plt.plot([0.0], [1.22], 'xb', mew=3, ms=10)
+# plt.plot([0.0], [1.22], 'xb', mew=3, ms=10)
 
-ok = n_edge >= np.sqrt(vals['n_fail'])
-plt.plot(xvals[ok], yvals[ok], '.', mfc='r', mec='r',
-         label='Number edge failures >= sqrt(n_fail)')
-plt.xlim(-1, 22)
+plt.xlim(-0.05, 5.0)
+plt.ylim(-0.05, 4.0)
 plt.grid()
-plt.xlabel("Actuator failures")
-plt.ylabel('HPD (arcsec)')
-plt.title("HPD vs. actuator failures in manufacturing")
-plt.legend(loc='best')
+plt.xlabel("Actuator failure rate (percent)")
+plt.ylabel('HPD relative to zero-failure case')
+plt.tight_layout()
 
-plt.savefig('reports/10+0_half_exemplar/1/act_fail_manuf.png')
+plt.savefig('reports/10+0_half_exemplar/21/act_fail_manuf.png')
